ad7fcd4f71720dbffdaab152086e6876faf11fb5
Alex Anderson <alex.anderson@my.uwrf.edu>
2023-04-18 16:08:14 -0500

    Milestone4

---
+++ BoxPut/maps/Tiles.tsx
@@ -0,0 +1,10 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.5" name="Tiles" tilewidth="70" tileheight="70" tilecount="2" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="70" height="70" source="platformerGraphicsDeluxe_Updated/Tiles/castleCenter.png"/>
+ </tile>
+ <tile id="1">
+  <image width="70" height="70" source="platformerGraphicsDeluxe_Updated/Tiles/dirtCenter.png"/>
+ </tile>
+</tileset>
--- BoxPut/src/main/java/p1/Program.java
+++ BoxPut/src/main/java/p1/Program.java
@@ -1,11 +1,27 @@
 package p1;
 import de.gurkenlabs.litiengine.Game;
+import de.gurkenlabs.litiengine.resources.Resources;
 
 public class Program {
 
   public static void main(String[] args) {
+	  
+	  //sets information about the game 
+	  Game.info().setName("BoxPut"); 
+	  Game.info().setSubTitle("");
+	  Game.info().setVersion("v0.1"); 
+	  Game.info().setWebsite(""); 
+	  Game.info().setDescription("golf but box");
+	  
+	  
 	  // initializes the Litiengine infrastructure
 	  Game.init(args);
+
+	  // loads data from the utiLITI game file 
+	  Resources.load("game.litidata"); 
+
+	  // loads the first level
+	  Game.world().loadEnvironment("level1"); 
 	  //starts the game
 	  Game.start();
   }
